8bed4a5b817ec6248df38622651911a66d8cfd00
julio-miguel <69321006+julio-miguel@users.noreply.github.com>
2020-10-01 17:14:24 -0600

    adicion de el formulario Cargo

---
+++ Planilla/Formularios/frmCargo.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Planilla.Formularios
+{
+    partial class frmCargo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCargo));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chkCargo = new System.Windows.Forms.CheckBox();
+            this.chkIdentificador = new System.Windows.Forms.CheckBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbFiltrar = new System.Windows.Forms.ToolStripButton();
+            this.tsbNuevo = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tsbFiltroAutomatico = new System.Windows.Forms.ToolStripButton();
+            this.tsbSeleccionarTodos = new System.Windows.Forms.ToolStripButton();
+            this.tsbMarcarTodos = new System.Windows.Forms.ToolStripButton();
+            this.tsbNoRegistros = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Panel2.Controls.Add(this.statusStrip1);
+            this.splitContainer1.Panel2.Controls.Add(this.dgvLista);
+            this.splitContainer1.Size = new System.Drawing.Size(499, 378);
+            this.splitContainer1.SplitterDistance = 115;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.chkCargo);
+            this.groupBox1.Controls.Add(this.chkIdentificador);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informacion de cargos del empleado";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(277, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // chkCargo
+            // 
+            this.chkCargo.AutoSize = true;
+            this.chkCargo.Location = new System.Drawing.Point(67, 71);
+            this.chkCargo.Name = "chkCargo";
+            this.chkCargo.Size = new System.Drawing.Size(57, 17);
+            this.chkCargo.TabIndex = 1;
+            this.chkCargo.Text = "Cargo:";
+            this.chkCargo.UseVisualStyleBackColor = true;
+            this.chkCargo.CheckedChanged += new System.EventHandler(this.chkCargo_CheckedChanged);
+            // 
+            // chkIdentificador
+            // 
+            this.chkIdentificador.AutoSize = true;
+            this.chkIdentificador.Location = new System.Drawing.Point(67, 32);
+            this.chkIdentificador.Name = "chkIdentificador";
+            this.chkIdentificador.Size = new System.Drawing.Size(87, 17);
+            this.chkIdentificador.TabIndex = 0;
+            this.chkIdentificador.Text = "Identificador:";
+            this.chkIdentificador.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbFiltroAutomatico,
+            this.tsbFiltrar,
+            this.tsbNuevo,
+            this.toolStripSeparator1,
+            this.tsbMarcarTodos,
+            this.tsbSeleccionarTodos});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(499, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbFiltrar
+            // 
+            this.tsbFiltrar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbFiltrar.Image = ((System.Drawing.Image)(resources.GetObject("tsbFiltrar.Image")));
+            this.tsbFiltrar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbFiltrar.Name = "tsbFiltrar";
+            this.tsbFiltrar.Size = new System.Drawing.Size(23, 22);
+            this.tsbFiltrar.Text = "toolStripButton1";
+            // 
+            // tsbNuevo
+            // 
+            this.tsbNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNuevo.Image")));
+            this.tsbNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNuevo.Name = "tsbNuevo";
+            this.tsbNuevo.Size = new System.Drawing.Size(23, 22);
+            this.tsbNuevo.Text = "toolStripButton1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNoRegistros});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 233);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(499, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Location = new System.Drawing.Point(12, 38);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.Size = new System.Drawing.Size(471, 192);
+            this.dgvLista.TabIndex = 0;
+            // 
+            // tsbFiltroAutomatico
+            // 
+            this.tsbFiltroAutomatico.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.tsbFiltroAutomatico.Image = global::Planilla.Properties.Resources.checked16x16;
+            this.tsbFiltroAutomatico.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbFiltroAutomatico.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbFiltroAutomatico.Name = "tsbFiltroAutomatico";
+            this.tsbFiltroAutomatico.Size = new System.Drawing.Size(120, 22);
+            this.tsbFiltroAutomatico.Text = "Filtro Automatico";
+            // 
+            // tsbSeleccionarTodos
+            // 
+            this.tsbSeleccionarTodos.Image = global::Planilla.Properties.Resources.checked16x16;
+            this.tsbSeleccionarTodos.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbSeleccionarTodos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSeleccionarTodos.Name = "tsbSeleccionarTodos";
+            this.tsbSeleccionarTodos.Size = new System.Drawing.Size(87, 22);
+            this.tsbSeleccionarTodos.Text = "Seleccionar";
+            // 
+            // tsbMarcarTodos
+            // 
+            this.tsbMarcarTodos.Image = global::Planilla.Properties.Resources.checked16x16;
+            this.tsbMarcarTodos.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbMarcarTodos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbMarcarTodos.Name = "tsbMarcarTodos";
+            this.tsbMarcarTodos.Size = new System.Drawing.Size(93, 22);
+            this.tsbMarcarTodos.Text = "Marca todos";
+            // 
+            // tsbNoRegistros
+            // 
+            this.tsbNoRegistros.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tsbNoRegistros.Name = "tsbNoRegistros";
+            this.tsbNoRegistros.Size = new System.Drawing.Size(136, 21);
+            this.tsbNoRegistros.Text = "No. de registros: 0";
+            // 
+            // frmCargo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 378);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmCargo";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox chkCargo;
+        private System.Windows.Forms.CheckBox chkIdentificador;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbFiltrar;
+        private System.Windows.Forms.ToolStripButton tsbNuevo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsbFiltroAutomatico;
+        private System.Windows.Forms.ToolStripButton tsbSeleccionarTodos;
+        private System.Windows.Forms.ToolStripButton tsbMarcarTodos;
+        private System.Windows.Forms.ToolStripStatusLabel tsbNoRegistros;
+    }
+}
---
+++ Planilla/Formularios/frmCargo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Planilla.Formularios
+{
+    public partial class frmCargo : Form
+    {
+        public frmCargo()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void chkCargo_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
