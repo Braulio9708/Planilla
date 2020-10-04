421de642df3a201991cd3e9ffbfedd3597f0acab
julio-miguel <69321006+julio-miguel@users.noreply.github.com>
2020-10-04 10:05:20 -0600

    creacion de formulario usuario operacion y confirmacion de sincronizacion

---
+++ Planilla/Formularios/frmUsuarioOperacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Planilla.Formularios
+{
+    public partial class frmUsuarioOperacion : Form
+    {
+        public frmUsuarioOperacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
